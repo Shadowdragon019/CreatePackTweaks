5d4051ad86f46c6366874166a2a8ab5f64a822e8
Sammy [any/all] <Shadowdragon019019@gmail.com>
2024-09-04 13:35:58 -0500

    Adding wrenching & goggles to the default player

--- src/main/java/com/roxxane/create_pack_tweaks/Cpt.java
+++ src/main/java/com/roxxane/create_pack_tweaks/Cpt.java
@@ -5,8 +5,15 @@ import com.roxxane.create_pack_tweaks.blocks.CptBlocks;
 import com.roxxane.create_pack_tweaks.blocks.entities.CptBlockEntities;
 import com.roxxane.create_pack_tweaks.items.CptItems;
 import com.roxxane.create_pack_tweaks.worldgen.features.CptFeatures;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.equipment.goggles.GogglesItem;
 import com.tterrag.registrate.Registrate;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -29,12 +36,27 @@ public class Cpt {
         CptBlockEntities.register();
         CptFeatures.register(modEventBus);
 
+        MinecraftForge.EVENT_BUS.addListener(this::wrench);
         registrate.addRawLang("chat.create_pack_tweaks.mold_contains", "§8Goes in molds:");
         registrate.addRawLang("chat.create_pack_tweaks.mold_contains_ingot", "§8 - Ingot");
         registrate.addRawLang("category.create_pack_tweaks.mold_heating", "Mold Heating");
         registrate.addRawLang("category.create_pack_tweaks.drilling", "Drilling");
         registrate.addRawLang("chat.create_pack_tweaks.reload_error", "§c" + displayName + " failed to reload!");
 
+        GogglesItem.addIsWearingPredicate((player) -> true);
+    }
+
+    public void wrench(PlayerInteractEvent.RightClickBlock event) {
+        var level = event.getLevel();
+        var player = event.getEntity();
+        var hand = event.getHand();
+        var item = event.getEntity().getItemInHand(hand);
+        var hitResult = event.getHitVec();
+
+        if (item.isEmpty() && hand == InteractionHand.MAIN_HAND && player.isShiftKeyDown())
+            if (AllItems.WRENCH.get().useOn(new UseOnContext(level, player, hand, item, hitResult))
+                == InteractionResult.SUCCESS)
+                player.swing(hand);
     }
 
     public static ResourceLocation resourceLocation(String path) {
