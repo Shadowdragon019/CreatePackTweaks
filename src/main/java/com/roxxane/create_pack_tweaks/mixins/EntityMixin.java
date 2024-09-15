e93cc5e0447b85f9b3e9c5ebabfd98d83d8a7462
Sammy [any/all] <Shadowdragon019019@gmail.com>
2024-09-15 15:01:38 -0500

    Custom movement only for player

--- src/main/java/com/roxxane/create_pack_tweaks/mixins/EntityMixin.java
+++ src/main/java/com/roxxane/create_pack_tweaks/mixins/EntityMixin.java
@@ -1,8 +1,10 @@
 package com.roxxane.create_pack_tweaks.mixins;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,21 +14,29 @@ import java.util.function.Predicate;
 
 @Mixin(Entity.class)
 abstract class EntityMixin {
+    @Unique
+    private boolean cpt$isPlayer() {
+        return (Entity) (Object) this instanceof Player;
+    }
+
     // Disable swimming
     @Inject(method = "isSwimming", at = @At("HEAD"), cancellable = true)
     private void isSwimmingInject(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+        if (cpt$isPlayer())
+            cir.setReturnValue(false);
     }
 
     // Nuke the hell out of liquid physics
     @Inject(method = "updateFluidHeightAndDoFluidPushing(Ljava/util/function/Predicate;)V",
         at = @At("HEAD"), cancellable = true, remap = false)
     private void updateFluidHeightAndDoFluidPushingInject(Predicate<FluidState> shouldUpdate, CallbackInfo ci) {
-        ci.cancel();
+        if (cpt$isPlayer())
+            ci.cancel();
     }
 
     @Inject(method = "isSprinting", at = @At("HEAD"), cancellable = true)
     private void isSprintingInject(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+        if (cpt$isPlayer())
+            cir.setReturnValue(false);
     }
 }
--- src/main/java/com/roxxane/create_pack_tweaks/mixins/LivingEntityMixin.java
+++ src/main/java/com/roxxane/create_pack_tweaks/mixins/LivingEntityMixin.java
@@ -1,19 +1,34 @@
 package com.roxxane.create_pack_tweaks.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.entity.Attackable;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.extensions.IForgeLivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
-abstract class LivingEntityMixin {
+abstract class LivingEntityMixin extends Entity implements Attackable, IForgeLivingEntity {
+    public LivingEntityMixin(EntityType<?> p_19870_, Level p_19871_) {
+        super(p_19870_, p_19871_);
+    }
+
+    @Unique
+    private boolean cpt$isPlayer() {
+        return (LivingEntity) (Object) this instanceof Player;
+    }
+
     @ModifyConstant(method = "getJumpPower", constant = @Constant(floatValue = 0.42f))
     private float jumpFromGroundModifyConstant(float original) {
-        if (((LivingEntity) (Object) this) instanceof Player)
+        if (cpt$isPlayer())
             return 0.85f;
         else
             return original;
@@ -21,11 +36,17 @@ abstract class LivingEntityMixin {
 
     @Redirect(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;onGround()Z", ordinal = 2))
     private boolean travelRedirect(LivingEntity instance, @Local(name = "f2") float f2) {
-        return true;
+        if (cpt$isPlayer())
+            return true;
+        else
+            return onGround();
     }
 
     @Redirect(method = "getFrictionInfluencedSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;onGround()Z"))
     private boolean getFrictionInfluencedSpeedRedirect(LivingEntity instance) {
-        return true;
+        if (cpt$isPlayer())
+            return true;
+        else
+            return onGround();
     }
 }
