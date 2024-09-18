3e9db7481ed3340c6b01d64feb42ab614b311a3f
Sammy [any/all] <Shadowdragon019019@gmail.com>
2024-09-18 15:32:38 -0500

    Not use an Overwrite for custom liquid rendering & add Embeddium support

--- src/main/java/com/roxxane/create_pack_tweaks/mixins/embeddium/FluidRendererMixin.java
+++ src/main/java/com/roxxane/create_pack_tweaks/mixins/embeddium/FluidRendererMixin.java
@@ -1,8 +1,8 @@
-package com.roxxane.create_pack_tweaks.mixins;
+package com.roxxane.create_pack_tweaks.mixins.embeddium;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.renderer.block.LiquidBlockRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.FluidRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.material.FluidState;
@@ -10,11 +10,11 @@ import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(LiquidBlockRenderer.class)
-public abstract class LiquidBlockRendererMixin {
-    @WrapOperation(method = "tesselate", at = @At(value = "INVOKE",
+@Mixin(FluidRenderer.class)
+public class FluidRendererMixin {
+    @WrapOperation(method = "render", at = @At(value = "INVOKE",
         target = "Lnet/minecraft/world/level/material/FluidState;getFlow(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;"))
-    private Vec3 tesselateWrapOperation(FluidState instance, BlockGetter pLevel, BlockPos pPos, Operation<Vec3> original) {
+    private Vec3 renderWrapOperation(FluidState instance, BlockGetter level, BlockPos pos, Operation<Vec3> original) {
         return Vec3.ZERO;
     }
 }
