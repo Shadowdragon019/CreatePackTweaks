33a0d2eda092605f9e19092be0333514c1e01ab1
Roxxane <>
2025-04-20 14:34:11 -0500

    Drills now spawn items at their back instead

--- src/main/java/com/roxxane/create_pack_tweaks/mixins/BlockBreakingKineticBlockEntityMixin.java
+++ src/main/java/com/roxxane/create_pack_tweaks/mixins/BlockBreakingKineticBlockEntityMixin.java
@@ -7,9 +7,9 @@ import com.simibubi.create.content.kinetics.drill.DrillBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,54 +19,61 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBreakingKineticBlockEntity.class)
 abstract class BlockBreakingKineticBlockEntityMixin extends KineticBlockEntity {
-    @Unique
-    int createAutomatizedMod$oreDropTimer = 0;
+	@Unique
+	int fct$ore_drop_timer = 0;
 
-    @Unique
-    int createAutomatizedMod$maxOreDropTimerSpeed = 200 * 16;
+	@Unique
+	final int fct$max_ore_drop_timer_speed = 200 * 16;
 
-    @Shadow(remap = false) protected BlockPos breakingPos;
+	@Shadow(remap = false) protected BlockPos breakingPos;
 
-    @Shadow(remap = false) protected abstract boolean shouldRun();
+	@Shadow(remap = false) protected abstract boolean shouldRun();
 
-    @Shadow(remap = false) protected int breakerId;
+	@Shadow(remap = false) protected int breakerId;
 
-    public BlockBreakingKineticBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
-        super(typeIn, pos, state);
-    }
+	public BlockBreakingKineticBlockEntityMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
+		super(typeIn, pos, state);
+	}
 
-    //this is weirdly but not noticeably buggy because I can't *remove* progress (no, setting one to -1 doesn't fix the bugs)
-    //to see what i mean, place an ore, place a drill & make it mine the ore, remove the power, then rotate the drill with a wrench or replace the block with any other (don't power it again)
-    //MC eventually removes all (presumably inactive) block breaking progresses after a while
-    @Inject(method = "tick", at = @At("HEAD"), remap = false)
-    private void tickInject(CallbackInfo ci) {
-        if ((BlockBreakingKineticBlockEntity) (Object) this instanceof DrillBlockEntity) {
-            if (level.isClientSide || getSpeed() == 0 || !shouldRun() || level == null || breakingPos == null)
-                return;
+	//this is weirdly but not noticeably buggy because I can't *remove* progress (no, setting one to -1 doesn't fix the bugs)
+	//to see what i mean, place an ore, place a drill & make it mine the ore, remove the power, then rotate the drill with a wrench or replace the block with any other (don't power it again)
+	//MC eventually removes all (presumably inactive) block breaking progresses after a while
+	@Inject(method = "tick", at = @At("HEAD"), remap = false)
+	private void tickInject(CallbackInfo ci) {
+		if ((BlockBreakingKineticBlockEntity) (Object) this instanceof DrillBlockEntity) {
+			if (level.isClientSide || getSpeed() == 0 || !shouldRun() || level == null || breakingPos == null)
+				return;
 
-            Block blockToBreak = level.getBlockState(breakingPos).getBlock();
-            if (!CptConfig.isLoaded() || CptConfig.drillingMap.get(blockToBreak) == null)
-                createAutomatizedMod$oreDropTimer = 0;
-            else {
-                createAutomatizedMod$oreDropTimer += (int) (1 * Math.abs(getSpeed()));
+			var block_to_break = level.getBlockState(breakingPos).getBlock();
+			if (!CptConfig.isLoaded() || CptConfig.drillingMap.get(block_to_break) == null)
+				fct$ore_drop_timer = 0;
+			else {
+				fct$ore_drop_timer += (int) (1 * Math.abs(getSpeed()));
 
-                if (createAutomatizedMod$oreDropTimer > createAutomatizedMod$maxOreDropTimerSpeed) {
-                    createAutomatizedMod$oreDropTimer = 0;
-                    level.addFreshEntity(new ItemEntity(level,
-                        breakingPos.getX() + 0.5,
-                        breakingPos.getY() + 0.5,
-                        breakingPos.getZ() + 0.5, new ItemStack(CptConfig.drillingMap.get(blockToBreak))));
-                }
-                level.destroyBlockProgress(breakerId, breakingPos,
-                    createAutomatizedMod$oreDropTimer / (createAutomatizedMod$maxOreDropTimerSpeed / 10));
-            }
-        }
-    }
+				if (fct$ore_drop_timer > fct$max_ore_drop_timer_speed) {
+					var drill_state = level.getBlockState(worldPosition);
+					var drop_item_pos =
+						worldPosition.relative(drill_state.getValue(BlockStateProperties.FACING), -1);
 
-    @Inject(method = "invalidate", at = @At("HEAD"), remap = false)
-    protected void invalidateInject(CallbackInfo ci) {
-        if (level != null && breakingPos != null) {
-            level.destroyBlockProgress(breakerId, breakingPos, -1);
-        }
-    }
+					level.addFreshEntity(new ItemEntity(level,
+						drop_item_pos.getX() + 0.5,
+						drop_item_pos.getY() + 0.5,
+						drop_item_pos.getZ() + 0.5,
+						new ItemStack(CptConfig.drillingMap.get(block_to_break))));
+
+					fct$ore_drop_timer = 0;
+				}
+				level.destroyBlockProgress(breakerId, breakingPos,
+					fct$ore_drop_timer / (fct$max_ore_drop_timer_speed / 10));
+			}
+		}
+	}
+
+	@Inject(method = "invalidate", at = @At("HEAD"), remap = false)
+	protected void invalidateInject(CallbackInfo ci) {
+		super.invalidate();
+		// TODO: Test
+		//if (level != null && breakingPos != null)
+		//level.destroyBlockProgress(breakerId, breakingPos, -1);
+	}
 }
